ebbad94d686f8bc1289a2477075c1ca1a29b13f3
pedromsantos <pedro.santos.personal@gmail.com>
2018-09-15 15:09:41 +0100

    Determine winner in rows

--- TicTacToe/Game.cs
+++ TicTacToe/Game.cs
@@ -1,12 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TicTacToe
 {
     public class Game
     {
         private char _lastSymbol = ' ';
-        private int _lastX = -1;
-        private int _lastY = -1;
+        private Dictionary<Tuple<int, int>, char> _plays = new Dictionary<Tuple<int, int>, char>();
 
         public void Play(char symbol, int x, int y)
         {
@@ -25,15 +25,63 @@ namespace TicTacToe
                 throw new Exception("Invalid next player");
             }
             //if not first move but play on an already played tile
-            else if (_lastX == x && _lastY == y)
+            else if (_plays.ContainsKey(new Tuple<int, int>(x, y)))
             {
                 throw new Exception("Invalid position");
             }
 
             // update game state
             _lastSymbol = symbol;
-            _lastX = x;
-            _lastY = y;
+            _plays[new Tuple<int, int>(x, y)] = symbol;
+        }
+
+        public char Winner()
+        {   //if the positions in first row are taken
+            if(_plays.ContainsKey(new Tuple<int, int>(0, 0)) &&
+               _plays.ContainsKey(new Tuple<int, int>(0, 1)) &&
+               _plays.ContainsKey(new Tuple<int, int>(0, 2)))
+               {
+                    //if first row is full with same symbol
+                    if (_plays[new Tuple<int, int>(0, 0)] == 
+                        _plays[new Tuple<int, int>(0, 1)] &&
+                        _plays[new Tuple<int, int>(0, 2)] == 
+                        _plays[new Tuple<int, int>(0, 1)] )
+                        {
+                            return _plays[new Tuple<int, int>(0, 0)];
+                        }
+               }
+                
+             //if the positions in first row are taken
+             if(_plays.ContainsKey(new Tuple<int, int>(1, 0)) &&
+                _plays.ContainsKey(new Tuple<int, int>(1, 1)) &&
+                _plays.ContainsKey(new Tuple<int, int>(1, 2)))
+                {
+                    //if middle row is full with same symbol
+                    if (_plays[new Tuple<int, int>(1, 0)] == 
+                        _plays[new Tuple<int, int>(1, 1)] &&
+                        _plays[new Tuple<int, int>(1, 2)] == 
+                        _plays[new Tuple<int, int>(1, 1)])
+                        {
+                            return _plays[new Tuple<int, int>(1, 0)];
+                        }
+                }
+
+            //if the positions in first row are taken
+             if(_plays.ContainsKey(new Tuple<int, int>(2, 0)) &&
+                _plays.ContainsKey(new Tuple<int, int>(2, 1)) &&
+                _plays.ContainsKey(new Tuple<int, int>(2, 2)))
+                {
+                    //if middle row is full with same symbol
+                    if (_plays[new Tuple<int, int>(2, 0)] == 
+                        _plays[new Tuple<int, int>(2, 1)] &&
+                        _plays[new Tuple<int, int>(2, 2)] == 
+                        _plays[new Tuple<int, int>(2, 1)])
+                        {
+                            return _plays[new Tuple<int, int>(2, 0)];
+                        }
+                }
+
+            return ' ';
         }
     }
 }
--- TicTacToeTests/GameShould.cs
+++ TicTacToeTests/GameShould.cs
@@ -6,11 +6,16 @@ namespace TicTacToeTests
 {
     public class GameShould
     {
+        private Game game;
+
+        public GameShould()
+        {
+           game = new Game();
+        }
+
         [Fact]
         public void NotAllowPlayerOToPlayFirst()
         {
-            var game =new Game();
-
             Action wrongPlay = () => game.Play('O', 0, 0);
 
             var exception = Assert.Throws<Exception>(wrongPlay);
@@ -20,7 +25,6 @@ namespace TicTacToeTests
         [Fact]
         public void NotAllowPlayerXToPlayTwiceInARow()
         {
-            var game =new Game();
             game.Play('X', 0, 0);
             
             Action wrongPlay = () => game.Play('X', 1, 0);
@@ -32,13 +36,111 @@ namespace TicTacToeTests
         [Fact]
         public void NotAllowPlayerToPlayInLastPlayedPosition()
         {
-            var game =new Game();
             game.Play('X', 0, 0);
 
             Action wrongPlay = () => game.Play('O', 0, 0);
 
             var exception = Assert.Throws<Exception>(wrongPlay);
             Assert.Equal("Invalid position", exception.Message);
+        }
+
+        [Fact]
+        public void NotAllowPlayerToPlayInAnyPlayedPosition()
+        {
+            game.Play('X', 0, 0);
+            game.Play('O', 1, 0);
+
+            Action wrongPlay = () => game.Play('X', 0, 0);
+
+            var exception = Assert.Throws<Exception>(wrongPlay);
+            Assert.Equal("Invalid position", exception.Message);
+        }
+
+        [Fact]
+        public void DeclarePlayerXAsAWinnerIfThreeInTopRow()
+        {
+            game.Play('X', 0, 0);
+            game.Play('O', 1, 0);
+            game.Play('X', 0, 1);
+            game.Play('O', 1, 1);
+            game.Play('X', 0, 2);
+
+            var winner = game.Winner();
+
+            Assert.Equal('X', winner);
+        }
+
+        [Fact]
+        public void DeclarePlayerOAsAWinnerIfThreeInTopRow()
+        {
+            game.Play('X', 2, 2);
+            game.Play('O', 0, 0);
+            game.Play('X', 1, 0);
+            game.Play('O', 0, 1);
+            game.Play('X', 1, 1);
+            game.Play('O', 0, 2);
+
+            var winner = game.Winner();
+
+            Assert.Equal('O', winner);
+        }
+
+        [Fact]
+        public void DeclarePlayerXAsAWinnerIfThreeInMiddleRow()
+        {
+            game.Play('X', 1, 0);
+            game.Play('O', 0, 0);
+            game.Play('X', 1, 1);
+            game.Play('O', 0, 1);
+            game.Play('X', 1, 2);
+
+            var winner = game.Winner();
+
+            Assert.Equal('X', winner);
+        }
+
+        [Fact]
+        public void DeclarePlayerOAsAWinnerIfThreeInMiddleRow()
+        {
+            game.Play('X', 0, 0);
+            game.Play('O', 1, 0);
+            game.Play('X', 2, 0);
+            game.Play('O', 1, 1);
+            game.Play('X', 2, 1);
+            game.Play('O', 1, 2);
+
+            var winner = game.Winner();
+
+            Assert.Equal('O', winner);
+        }
+
+        [Fact]
+        public void DeclarePlayerXAsAWinnerIfThreeInBottomRow()
+        {
+            game.Play('X', 2, 0);
+            game.Play('O', 0, 0);
+            game.Play('X', 2, 1);
+            game.Play('O', 0, 1);
+            game.Play('X', 2, 2);
+
+            var winner = game.Winner();
+
+            Assert.Equal('X', winner);
+        }
+
+        [Fact]
+        public void DeclarePlayerOAsAWinnerIfThreeInBottomRow()
+        {
+            game.Play('X', 0, 0);
+            game.Play('O', 2, 0);
+            game.Play('X', 1, 0);
+            game.Play('O', 2, 1);
+            game.Play('X', 1, 1);
+            game.Play('O', 2, 2);
+
+            var winner = game.Winner();
+
+            Assert.Equal('O', winner);
         }
     }
 }
