29bdb4b7a18d27d80491f16df7f3b14aba009f30
Marco Consolaro <marco@alcortech.net>
2025-03-18 09:05:12 +0100

    fixed packages

--- TypeScript/src/Game.ts
+++ TypeScript/src/Game.ts
@@ -2,8 +2,7 @@ export class Game {
   private _lastSymbol: string = ' ';
   private _board: Board = new Board();
 
-  // Long para,meter list
-  // Primtive obsession
+
   public Play(symbol: string, x: number, y: number): void {
     //if first move
     if (this._lastSymbol == ' ') {
@@ -17,9 +16,6 @@ export class Game {
       throw new Error('Invalid next player');
     }
     //if not first move but play on an already played tile
-
-    // Message chain
-    // Innapropriate intimacy
     else if (this._board.TileAt(x, y).Symbol != ' ') {
       throw new Error('Invalid position');
     }
